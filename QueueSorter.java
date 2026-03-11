522bf6f4ac7254da99ec00324449cf23b2d3e01b
Christabelle Halim <chritabellesawo15@gmail.com>
2026-03-11 16:13:40 +0800

    Update

--- QueueSorter.java
+++ QueueSorter.java
@@ -1,4 +1,6 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class QueueSorter {
      public static void main(String[] args) {
@@ -7,37 +9,36 @@ public class QueueSorter {
         String input = sc.nextLine();
 
         String[] t = input.split(" ");
-
+        int[] arr = new int[t.length];
+        for(int i = 0; i < t.length; i++) {
+            arr[i] = Integer.parseInt(t[i]);
+        }
 
         if(t.length < 1 || t.length > 1000) { 
             System.out.println("Jumlah input harus 1 sampai 1000");
             return;
         }
         Queue<Integer> queueLolos = new LinkedList<>();
+        Queue<Integer> queueGakLolos = new LinkedList<>();
 
-        int[] outside = new int[t.length];
-        int outsideIndex  = 0;
+        for(int i = 0; i < arr.length; i++) {
+            int num = arr[i];
+            if(num >= 28 && num <= 118) {
+                queueLolos.add(num);
+            } else{
+                queueGakLolos.add(num);
+            }
+        }
 
-        for(String token : t) {
-            int num = Integer.parseInt(token);
-    
-        if(num >= 28 && num <= 118) {
-            queueLolos.add(num);
-        }
-        else{
-            outside[outsideIndex] = num;
-            outsideIndex++;
-        }
-        }
         System.out.println("\nDaftar yang lolos:");
         while(!queueLolos.isEmpty()) {
             int umur = queueLolos.remove();
-            System.out.println("dipanggil: " + umur);
+            System.out.println(umur);
         }
-
         System.out.println("\nDaftar yang tidak lolos:");
-        for(int i = 0; i < outsideIndex; i++) {
-        System.out.println("ditolak: " + outside[i]);
+        while(!queueGakLolos.isEmpty()) {
+            int umur = queueGakLolos.remove();
+            System.out.println(umur);
         }
     }
 }
